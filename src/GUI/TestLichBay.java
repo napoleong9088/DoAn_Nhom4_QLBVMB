38c3d65371ae1c9d1e1cbe90f9e8fbeb2d8a16ef
Dang Khoa <napoleong9088@gmail.com>
2021-04-29 20:52:41 +0700

    v1.0

---
+++ src/GUI/TestLichBay.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestLichBay {
+
+	@Test
+	void test() {
+		Option op = new Option();
+	}
+
+}
